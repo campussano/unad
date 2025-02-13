0f570d12c08112e94a983716fed7d9d724408678
campussano <2023-0339@unad.edu.do>
2025-02-12 20:30:24 -0400

    Add files via upload
    Primer archivo del proyecto

---
+++ palindromo.py
@@ -0,0 +1,10 @@
+palabra = input('Dime una palabra: ').lower()
+
+palabra_reverse = palabra[::-1]
+
+print(palabra, "-", palabra_reverse)
+
+# if palabra == palabra[::-1]:
+#     #print('Es un Palíndromo')
+# else:
+#     print('No es un Palíndromo')
